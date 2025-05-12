2c6264e2de54d6888578477c675717f5856ca9c7
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-05-12 10:00:35 +0530

    first day update

--- array/first.dart
+++ array/first.dart
@@ -1,20 +1,29 @@
 //  List<int> numbers = [1, 9, 3, 7, 8, 2, 5, 4, 6]; compare the 2 numbers sum = 10 print paires
 void main() {
   List<int> numbers = [1, 9, 3, 7, 8, 2, 5, 4, 6];
-  int targetSum = 10;
-  findPairs(numbers, targetSum);
+  // int targetSum = 10;
+  program(numbers);
+  // findPairs(numbers, targetSum);
   print(numbers);
 }
 
-void findPairs(List<int> numbers, int targetSum) {
-  for (int i = 0; i < numbers.length; i++) {
-    for (int j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] == targetSum) {
-        print("Pair found: ${numbers[i]}, ${numbers[j]}");
-      }
+void program(numbers) {
+  for (int i = 0; i <= numbers.length; i++) {
+    for (int j = i + 2; j <= numbers.length; j++) {
+      var multifpyde = numbers[i] * numbers[j];
+      print("multifpyde: $multifpyde");
     }
   }
-  }
+}
+// void findPairs(List<int> numbers, int targetSum) {
+//   for (int i = 0; i < numbers.length; i++) {
+//     for (int j = i + 1; j < numbers.length; j++) {
+//       if (numbers[i] + numbers[j] == targetSum) {
+//         print("Pair found: ${numbers[i]}, ${numbers[j]}");
+//       }
+//     }
+//   }
+//   }
   // Set<int> seenNumbers = {};
   // List<List<int>> pairs = [];
 
